c03c5adcb7d46493da2618d0713ea44e9bd993fb
Janai Kerana <41949440+janaikerana96@users.noreply.github.com>
2022-08-07 21:43:55 +0000

    Usando POST com associações.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,5 @@
 class ContactsController < ApplicationController
-  before_action :set_contact, only: %i[ show update destroy ]
+  before_action :set_contact, only: %i[show update destroy]
 
   # GET /contacts
   # GET /contacts.json
@@ -43,13 +43,14 @@ class ContactsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_contact
-      @contact = Contact.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def contact_params
-      params.require(:contact).permit(:name, :email, :birtdate)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_contact
+    @contact = Contact.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def contact_params
+    params.require(:contact).permit(:name, :email, :birtdate, :kind_id)
+  end
 end
