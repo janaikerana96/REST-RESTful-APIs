0f23009de03928c688f4665fb0c29d1beb7a422b
Janai Kerana <41949440+janaikerana96@users.noreply.github.com>
2022-08-07 21:30:05 +0000

    Render json com associações

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,14 @@
 class Contact < ApplicationRecord
   belongs_to  :kind
+  def author
+    'Janai Kerana'
+  end
+
+  def as_json(_options = {})
+    super(
+      methods: :author,
+      root: true,
+      include: { kind: { only: :descripton } }
+    )
+  end
 end
